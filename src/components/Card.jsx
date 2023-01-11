a66cb946d64862e9d91974043646bca309bdbb8b
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-01-11 04:12:41 -0800

    initial files

---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+import star from '../imgs/star.png'
+
+export default function Card(props) {
+    let badgeText
+    if (props.card.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.card.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    return (
+        <section className='card'>
+            {badgeText && <div className='card-badge'>{badgeText}</div>}
+            <img src={`src/imgs/${props.card.coverImg}`} alt="Katie Zaferes Smiling" className='card-image' />
+            <div className='card-details-container'>
+                <img src={star} alt="Red Star Icon" className='card-star-image' />
+                <span>{props.card.stats.rating} ({props.card.stats.reviewCount}) &#x2022; {props.card.location}</span>
+            </div>
+            <p className='card-description'>{props.card.title}</p>
+            <p className='card-price'><strong>From ${props.card.price}</strong> / person</p>
+        </section>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import airbnbLogo from '../imgs/airbnb-logo.png'
+
+export default function Navbar() {
+
+    return (
+        <nav>
+            <img src={airbnbLogo} alt="Airbnb Logo" />
+        </nav>
+    )
+}
